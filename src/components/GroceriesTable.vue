5b7f4c21d7f54a8630f05f21678a1c03013d4336
cheatze <frolicsomequipster@gmail.com>
2026-07-09 15:20:37 +0200

    Oude code verwijderd.

--- src/components/GroceriesTable.vue
+++ src/components/GroceriesTable.vue
@@ -1,13 +1,5 @@
 <script setup>
-//import HelloWorld from './components/HelloWorld.vue'
 import {ref, reactive} from 'vue';
-
-// const products = reactive([
-//     {id: 1, name: 'Rice', price: 1.0, quantity: 1},
-//     {id: 2, name: 'Broccoli', price: 0.99, quantity: 0},
-//     {id: 3, name: 'Cookies', price: 1.2, quantity: 0},
-//     {id: 4, name: 'Nuts', price: 2.99, quantity: 0},
-// ]);
 
 const props = defineProps({
     products: {
@@ -17,17 +9,6 @@ const props = defineProps({
 });
 
 const emit = defineEmits(['updateQuantity']);
-
-// function updateQuantity(productId, newQuantity) {
-//     emit('updateQuantity', productId, newQuantity);
-// }
-
-// function updateQuantity(productId, newQuantity) {
-//     const product = props.products.find(p => p.id === productId);
-//     if (product) {
-//         product.quantity = newQuantity;
-//     }
-// }
 </script>
 
 <template>
